3a586e30a2cd41bf4b05bd1b2ec38b29ad01ea93
Benoît Coste <benoit.coste@epfl.ch>
2020-12-23 10:29:33 +0100

    try something

--- test/quick_test.py
+++ test/quick_test.py
@@ -1,15 +1,5 @@
-status = False
-try:
-    import snap
-    version = snap.Version
-    i = snap.TInt(5)
-    if i == 5:
-        status = True
-except:
-    pass
-
-if status:
-    print("SUCCESS, your version of Snap.py is %s" % (version))
-else:
-    print("*** ERROR, no working Snap.py was found on your computer")
-
+import snap
+version = snap.Version
+i = snap.TInt(5)
+assert i == 5, "*** ERROR, no working Snap.py was found on your computer"
+print("SUCCESS, your version of Snap.py is %s" % (version))
